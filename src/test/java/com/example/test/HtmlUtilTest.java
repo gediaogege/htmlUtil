d1316d99a848b74886cd8cc4d19286ad0a5202b1
qinweiwei <qinweiwei0330@gmail.com>
2019-05-02 20:37:21 +0800

    add filterParams

--- src/test/java/com/example/test/HtmlUtilTest.java
+++ src/test/java/com/example/test/HtmlUtilTest.java
@@ -3,14 +3,11 @@ package com.example.test;
 import com.example.test.util.HtmlUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -24,5 +21,18 @@ public class HtmlUtilTest {
         List<String> strings = instance.getLinks(doc);
         List<String> outLinks = instance.filterOutLinks(strings);
         System.out.println(outLinks.toString());
+
+
+    }
+
+    @Test
+    public void filterLinks() {
+        HtmlUtil instance = HtmlUtil.getInstance();
+        String html = instance.readHtml("/home/qww/data/workspace/test/test/src/main/resources/static/test.html");
+        Document doc = Jsoup.parse(html);
+        List<String> strings = instance.getLinks(doc);
+        //根据条件过滤
+        List<String> links = instance.filterLinks(strings, true,".*\\d+.*");
+        System.out.println(links.toString());
     }
 }
